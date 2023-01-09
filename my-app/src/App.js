325a8513518f4bedf48a7c55270c09e40fe91f17
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-09 11:55:13 +0200

    Edit the paragraph in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Created a React application is using Create React App (CRA)
         </p>
         <a
           className="App-link"
