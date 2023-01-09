4f9b2a48d0c2c7ea66b6708bd5b625bd3071cbb2
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-09 12:32:49 +0200

    Fix linters errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Created a React application is using Create React App (CRA)
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
