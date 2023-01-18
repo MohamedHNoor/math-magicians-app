7eba3566e26e67701cc9b9e1a6860b6a0762ce80
Yash <74345713+yash244466666@users.noreply.github.com>
2023-01-18 15:52:49 +0600

    Add test for calculator component

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,51 @@
+import calculate from './calculate';
+describe('testing calculate function', () => {
+  test('should return total', () => {
+    expect(calculate({ total: '20', next: 5, operation: '+' }, '=')).toEqual({
+      total: '25',
+      next: null,
+      operation: null,
+    });
+    expect(calculate({ total: '20', next: 5, operation: '-' }, '=')).toEqual({
+      total: '15',
+      next: null,
+      operation: null,
+    });
+    expect(calculate({ total: '20', next: 5, operation: 'x' }, '=')).toEqual({
+      total: '100',
+      next: null,
+      operation: null,
+    });
+    expect(calculate({ total: '20', next: 5, operation: '÷' }, '=')).toEqual({
+      total: '4',
+      next: null,
+      operation: null,
+    });
+  });
+  test('should return empty object', () => {
+    expect(calculate({ total: '0', next: 5, operation: null }, '=')).toEqual(
+      {},
+    );
+  });
+  test('+/- button', () => {
+    expect(
+      calculate({ total: '10', next: '5', operation: null }, '+/-'),
+    ).toEqual({
+      total: '10',
+      next: '-5',
+      operation: null,
+    });
+    expect(
+      calculate({ total: '15', next: null, operation: null }, '+/-'),
+    ).toEqual({
+      total: '-15',
+      next: null,
+      operation: null,
+    });
+  });
+  test('for reset button', () => {
+    expect(
+      calculate({ total: '20', next: '10', operation: '+' }, 'AC'),
+    ).toEqual({ total: null, next: null, operation: null });
+  });
+});
